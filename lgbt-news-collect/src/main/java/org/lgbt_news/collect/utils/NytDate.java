af61f20b4aa8bef8dd66df720ab166491479dc94
macksimiljan <macksimiljan@users.noreply.github.com>
2017-02-10 18:33:01 +0100

    modelled date for nyt

---
+++ lgbt-news-collect/src/main/java/org/lgbt_news/collect/utils/NytDate.java
@@ -0,0 +1,58 @@
+package org.lgbt_news.collect.utils;
+
+
+/**
+ * @author max
+ */
+public class NytDate {
+
+    private int year;
+    private int month;
+    private int day;
+
+    private NytDate(Builder b) {
+        year = b.y;
+        month = b.m;
+        day = b.d;
+    }
+
+    public static class Builder {
+        private int y = 2017;
+        private int m = 1;
+        private int d = 1;
+
+        public Builder year(int val) {
+            if (val > 1000 && val < 9999)
+                y = val;
+            return this;
+        }
+
+        public Builder month(int val) {
+            if (val > 0 && val <= 12)
+                m = val;
+            return this;
+        }
+
+        public Builder day(int val) {
+            if (val > 0 && val <= 31)
+                d = val;
+            return this;
+        }
+
+        public NytDate createDate() {
+            return new NytDate(this);
+        }
+    }
+
+    public NytDate getEarliestDate() {
+        return new Builder().year(1851).month(9).day(18).createDate();
+    }
+
+    public String toString() {
+        String date = String.valueOf(year);
+        date += (month < 10) ? "0"+month : month;
+        date += (day < 10) ? "0"+day : day;
+
+        return date;
+    }
+}
