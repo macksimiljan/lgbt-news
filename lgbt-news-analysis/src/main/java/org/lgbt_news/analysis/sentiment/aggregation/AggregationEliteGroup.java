f2ca09bfdb233480d5478756047f8d0173fb2c3f
macksimiljan <macksimiljan@users.noreply.github.com>
2017-02-16 16:40:13 +0100

    implemented elite-group aggregation

---
+++ lgbt-news-analysis/src/main/java/org/lgbt_news/analysis/sentiment/aggregation/AggregationEliteGroup.java
@@ -0,0 +1,57 @@
+package org.lgbt_news.analysis.sentiment.aggregation;
+
+import org.lgbt_news.analysis.sentiment.SentimentCategory;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author max
+ */
+public class AggregationEliteGroup implements Aggregation {
+
+    private final int STRENGTH_OF_ELITE;
+
+    private Set<Integer> indicesOfElite;
+    private AggregationMajorityVote majorityVote;
+
+    public AggregationEliteGroup(int strengthOfElite) {
+        STRENGTH_OF_ELITE = (strengthOfElite < 2) ? 2 : strengthOfElite;
+        indicesOfElite = new HashSet<>();
+        majorityVote = new AggregationMajorityVote();
+    }
+
+    public void setIndicesOfElite(Set<Integer> indicesOfElite) {
+        this.indicesOfElite = indicesOfElite;
+    }
+
+    @Override
+    public SentimentCategory aggregateCategories(List<SentimentCategory> categories) {
+        for (int index : indicesOfElite) {
+            SentimentCategory eliteCategory = categories.get(index);
+            for (int i = 0; i < STRENGTH_OF_ELITE; i++) {
+                categories.add(eliteCategory);
+            }
+        }
+
+        return majorityVote.aggregateCategories(categories);
+    }
+
+    @Override
+    public SentimentCategory aggregatePredictions(List<double[]> predictions) {
+        for (int index : indicesOfElite) {
+            double[] elitePrediction = predictions.get(index);
+            for (int i = 0; i < STRENGTH_OF_ELITE; i++) {
+                predictions.add(elitePrediction);
+            }
+        }
+
+        return majorityVote.aggregatePredictions(predictions);
+    }
+
+    @Override
+    public double getProbability() {
+        return majorityVote.getProbability();
+    }
+}
